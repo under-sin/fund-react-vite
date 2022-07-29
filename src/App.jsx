03c91b1d0c254933a7806dbef0c696ff340d9395
anderson <andersonvieira818@yahoo.com.br>
2022-07-29 08:46:47 -0300

    style: add global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { Post } from "./Post";
+import {Header} from "./components/Header";
+
+import './global.css'
 
 export function App() {
   return (
     <>
+      <Header />
       <Post
         author="Anderson"
         content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum ducimus veritatis quod nulla voluptatem, quaerat reiciendis enim quis aperiam quia impedit nostrum eveniet laboriosam error excepturi at, rem officiis a."
       />
 
       <Post author="Diego" content="Novo post" />
+      <Post />
     </>
   );
 }
